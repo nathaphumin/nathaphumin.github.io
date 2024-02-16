e5db514ddc4344c3dc4d91cba2c969ad2011e13f
Your Name <natthaphumin.wi.66@ubu.ac.th>
2024-02-16 22:21:23 +0700

    first commit

---
+++ streamlit/app02.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.sidebar.text('Sidebar')
+st.sidebar.button('go>')
+st.sidebar.button('went>')
+st.sidebar.button('gone>')
+st.title('OOP in ML')
+st.image('heeps://picsum.photos/500/200')
+st.text_input('What s your neme?')
